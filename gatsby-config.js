9ee794070bec6a872b64da35c08ef4f7393b9ccb
Jhon <jhon_oropeza810otmail.com>
2021-01-20 18:17:02 -0600

    Change theme color for mobile

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,8 +30,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#bc9a1a`,
+        theme_color: `#000`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
